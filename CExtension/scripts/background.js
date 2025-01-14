f086238d501f41557e2765a51e506f245edbf81d
Priyam Tomar <priyamtomar012@gmail.com>
2025-01-14 13:29:38 +0530

    Acaderealty deployed

--- CExtension/scripts/background.js
+++ CExtension/scripts/background.js
@@ -55,7 +55,7 @@ function authenticateWithGoogle(sender) {
 }
 
 function sendAuthCodeToBackend(authCode, sender) {
-  fetch("http://localhost:5000/auth", {
+  fetch("https://acaderealty.com/auth", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
